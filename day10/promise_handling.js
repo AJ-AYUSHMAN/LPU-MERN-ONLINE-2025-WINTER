09603b1e15cc7f1a3ddccf7f2f9138ddb031c578
Likhilesh <likbalpande@example.com>
2025-01-24 18:00:28 +0530

    js promises WITH more examples

--- day10/promise_handling.js
+++ day10/promise_handling.js
@@ -26,23 +26,52 @@
 
 // ----------------------------------SHORT WAY (CODE)----------------------------------------------
 
+// console.log("START");
+// const request = fetch("https://www.redbus.in/seowapi/offers");
+
+// request
+//     .then((val) => {
+//         console.log("LEVEL 1 - START", val);
+//         const pr = val.json();
+//         console.log("LEVEL 1 - MID", val);
+//         pr.then((val) => {
+//             console.log("YES YES", val);
+//         }).catch((err) => {
+//             console.log("NO", err);
+//         });
+//         console.log("LEVEL 1 - END", val);
+//     })
+//     .catch((err) => {
+//         console.log("NO", err);
+//     });
+
+// console.log("END");
+
+// ------------------------- EXPANDED CODE ----------------------------
+
 console.log("START");
 const request = fetch("https://www.redbus.in/seowapi/offers");
 
-request
-    .then((val) => {
-        console.log("LEVEL 1 - START", val);
-        const pr = val.json();
-        console.log("LEVEL 1 - MID", val);
-        pr.then((val) => {
-            console.log("YES YES", val);
-        }).catch((err) => {
-            console.log("NO", err);
-        });
-        console.log("LEVEL 1 - END", val);
-    })
-    .catch((err) => {
-        console.log("NO", err);
-    });
+const ff1 = (val) => {
+    console.log("YES YES", val);
+};
+
+const ff2 = (err) => {
+    console.log("NO", err);
+};
+
+const fn1 = (val) => {
+    console.log("LEVEL 1 - START", val);
+    const pr = val.json();
+    console.log("LEVEL 1 - MID", val);
+    pr.then(ff1).catch(ff2);
+    console.log("LEVEL 1 - END", val);
+};
+
+const fn2 = (err) => {
+    console.log("NO", err);
+};
+
+request.then(fn1).catch(fn2);
 
 console.log("END");
