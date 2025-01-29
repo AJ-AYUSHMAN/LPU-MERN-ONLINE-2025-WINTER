9df58ce9f84be1e41164565a4f2a19008eef1dda
Likhilesh <likbalpande@example.com>
2025-01-29 11:57:21 +0530

    infinite or ZERO - API CALL

--- day14-useEffect_PromiseHandling/app.js
+++ day14-useEffect_PromiseHandling/app.js
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Dashboard from "./src/pages/dashboard";
+import DummyTodos from "./src/pages/dummyTodos";
 
 const domElement = document.getElementById("dom-root");
 const reactRoot = ReactDOM.createRoot(domElement);
@@ -10,7 +11,8 @@ const App = () => {
     return (
         // <React.Fragment>
         <>
-            <Dashboard />
+            {/* <Dashboard /> */}
+            <DummyTodos />
         </>
         // </React.Fragment>
     );
