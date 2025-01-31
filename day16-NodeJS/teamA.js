5beca5ddff27f8c22ed4fb476615eaeac3ea491d
Likhilesh <likbalpande@example.com>
2025-01-31 11:45:43 +0530

    import syntax using destructuring

--- day16-NodeJS/teamA.js
+++ day16-NodeJS/teamA.js
@@ -1,7 +1,7 @@
-const box = require("./teamB.js");
+const { sum, mul } = require("./teamB.js");
 
-const ans1 = box.sum(10, 20);
+const ans1 = sum(10, 20);
 console.log(ans1);
 
-const ans2 = box.mul(10, 20);
+const ans2 = mul(10, 20);
 console.log(ans2);
