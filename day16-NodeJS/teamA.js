6d63f383ff0570c1d34eaf0e395e4474ff28ceb8
Likhilesh <likbalpande@example.com>
2025-01-31 11:17:25 +0530

    started nodejs, done with single export in CJS

---
+++ day16-NodeJS/teamA.js
@@ -0,0 +1,4 @@
+const add = require("./teamB.js");
+
+const ans = add(10, 20); // arguments
+console.log(ans);
---
+++ day16-NodeJS/teamB.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+    // parameters
+    console.log("---calculating sum---");
+    return a + b;
+}
+
+module.exports = sum;
