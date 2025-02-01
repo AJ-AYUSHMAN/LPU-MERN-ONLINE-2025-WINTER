458d2671dc551a2707537225708fae009262b121
Likhilesh <likbalpande@example.com>
2025-02-01 09:36:58 +0530

    how does require work ::

---
+++ day17-NodeJS/teamB.js
@@ -0,0 +1,12 @@
+// this is your custom module - user defined module
+
+console.log("A");
+
+const sum = (a, b) => {
+    console.log("E");
+
+    console.log("---calculating sum---");
+    return a + b;
+};
+
+console.log("B");
