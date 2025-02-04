f685c89bdae7176eb2ab8bb777c9b04b12d2689d
Likhilesh <likbalpande@example.com>
2025-02-04 11:45:48 +0530

    POST & PATCH with proper ID

--- day19/app_v2.js
+++ day19/app_v2.js
@@ -11,35 +11,29 @@ app.get("/", (req, res) => {
     res.send(`<h1>Server is running on PORT : ${PORT}</h1>`);
 });
 
-app.get("/tasks", async (req, res) => {
-    try {
-        const text = await fsPromises.readFile("./db.json");
-        const obj = JSON.parse(text);
-        res.status(200);
-        res.json({
-            status: "success",
-            data: {
-                tasks: obj,
-            },
-        });
-    } catch (err) {
-        res.status(500);
-        res.json({
-            status: "fail",
-            message: "Internal Server Error",
-        });
-    }
-});
-
+// CREATE
 app.post("/tasks", async (req, res) => {
     try {
         // 1. you will get the data in request
         const newObj = req.body;
-        console.log("newObj:", newObj);
 
         // 2. read the current list
-        const text = await fsPromises.readFile("./db.json", "utf-8");
+        let text = await fsPromises.readFile("./db.json", "utf-8");
+        if (text.length == 0) text = "[]";
         const arr = JSON.parse(text);
+
+        // ------- ---------------------------------
+        // 2.1 we have to generate the id
+        let newId = 1;
+        if (arr.length !== 0) {
+            const lastTask = arr[arr.length - 1];
+            newId = lastTask.id;
+            newId += 1;
+        }
+
+        // 2.2 assign this new id to new object
+        newObj.id = newId;
+        // ------- ---------------------------------
 
         // 3. then append the new data into it
         arr.push(newObj);
@@ -53,6 +47,84 @@ app.post("/tasks", async (req, res) => {
             status: "success",
         });
     } catch (err) {
+        console.log("Error in POST TASKS: ", err.message);
+        res.status(500);
+        res.json({
+            status: "fail",
+            message: "Internal Server Error",
+        });
+    }
+});
+
+// READ
+app.get("/tasks", async (req, res) => {
+    try {
+        const text = await fsPromises.readFile("./db.json");
+        const obj = JSON.parse(text);
+        res.status(200);
+        res.json({
+            status: "success",
+            data: {
+                tasks: obj,
+            },
+        });
+    } catch (err) {
+        console.log("Error in GET TASKS: ", err.message);
+        res.status(500);
+        res.json({
+            status: "fail",
+            message: "Internal Server Error",
+        });
+    }
+});
+
+// UPDATE
+app.patch("/tasks/:taskId", async (req, res) => {
+    try {
+        // 1. Get data from request :: body for changes &
+        // we need which data object to change :: request params in the url
+        // --> identification mark :: id
+        const { taskId } = req.params;
+        const updatedTaskInfo = req.body;
+
+        // 2. Read the current List from file
+        const text = await fsPromises.readFile("./db.json", "utf-8");
+        const arr = JSON.parse(text);
+
+        // 3. Find the data object from the array that you want to update (if valid id)
+        const foundIndex = arr.findIndex((elem) => {
+            if (elem.id == taskId) {
+                return true;
+            }
+            return false;
+        });
+
+        if (foundIndex == -1) {
+            res.status(400);
+            res.json({
+                status: "fail",
+                message: "Invalid Task Id!",
+            });
+        } else {
+            // 4. change the data that you need to
+            const oldTask = arr[foundIndex];
+            const finalNewTask = { ...oldTask, updatedTaskInfo };
+            arr[foundIndex] = finalNewTask;
+
+            // 5. save the updated List as text in the file
+            const textData = JSON.stringify(arr);
+            await fsPromises.writeFile("./db.json", textData);
+
+            res.status(200);
+            res.json({
+                status: "success",
+                data: {
+                    task: finalNewTask,
+                },
+            });
+        }
+    } catch (err) {
+        console.log("Error in PATCH TASKS: ", err.message);
         res.status(500);
         res.json({
             status: "fail",
