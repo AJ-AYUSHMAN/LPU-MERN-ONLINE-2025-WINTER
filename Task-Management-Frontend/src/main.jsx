f389c07c04c889e895181e2bbc0f0a821295b100
Likhilesh <likbalpande@example.com>
2025-02-06 10:49:47 +0530

    basic react app

--- Task-Management-Frontend/src/main.jsx
+++ Task-Management-Frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx"; // ES6 Module (ESM)
+// const App = require('./App.jsx') // Common JS Module (CJS)
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const domRoot = document.getElementById("root");
+const reactRoot = ReactDOM.createRoot(domRoot);
+
+reactRoot.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
